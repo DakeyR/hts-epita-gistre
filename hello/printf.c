4110e36ff50d6f9701943f61d679938c8454d0e0
Raphael Gault <raphael.gault@epita.fr>
2018-02-05 15:16:18 +0100

    hello: Retrieve Phdr and Dynamic segment

--- hello/printf.c
+++ hello/printf.c
@@ -4,5 +4,27 @@ void *get_auxv(char *envp[])
 {
   while (envp && *envp)
     envp++;
-  return envp;
+  return !*envp ? envp++: envp;
+}
+
+void *get_phdr(ElfW(auxv_t) *auxv)
+{
+  while (auxv && auxv->a_type != AT_NULL)
+  {
+    if (auxv->a_type == AT_PHDR)
+      return (void *)auxv->a_un.a_val;
+    auxv++;
+  }
+  return (void *)0;
+}
+
+void *get_dyn(ElfW(Phdr) *phdr)
+{
+  while (phdr && phdr->p_type != PT_NULL)
+  {
+    if (phdr->p_type == PT_DYNAMIC)
+      return (void *)phdr->p_vaddr;
+    phdr++;
+  }
+  return (void *)0;
 }
--- hello/printf.h
+++ hello/printf.h
@@ -1,4 +1,7 @@
 #include <link.h>
 
 void *get_auxv(char *envp[]);
+void *get_phdr(ElfW(auxv_t) *auxv);
+void *get_dyn(ElfW(Phdr) *phdr);
+void *get_rdebug(ElfW(Dyn) *dyn);
 int call_printf(const char *format, ...);
