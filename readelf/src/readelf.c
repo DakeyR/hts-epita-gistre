03b9eabcf3887570b4c28ab95e768c14481839c2
Raphael Gault <raphael.gault@epita.fr>
2018-02-06 16:06:38 +0100

    readelf: Load binary into memory

--- readelf/src/readelf.c
+++ readelf/src/readelf.c
@@ -1,7 +1,42 @@
+#include <sys/mman.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "readelf.h"
 
-int main()
+void *load_header(int fd, off_t size)
 {
-  printf("Simple Readelf\n");
+  void *ptr = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+  return ptr == MAP_FAILED ? NULL : ptr;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    fprintf(stderr, "simple-readelf requires exactly 1 argument, %d provided\n%s",
+            argc - 1, "Usage: simple-readelf BINARY\n");
+    return 1;
+  }
+
+  printf("Binary path: %s\n", argv[1]);
+
+  int fd = open(argv[1], O_RDONLY);
+  if (fd == -1)
+  {
+    fprintf(stderr, "simple-readelf: error: impossible to open %s\n", argv[1]);
+    return 2;
+  }
+
+  struct stat buf;
+  if (fstat(fd, &buf) == -1)
+    return 3;
+
+  ElfW(Ehdr) *header = load_header(fd, buf.st_size);
+
+  munmap(header, buf.st_size);
+
   return 0;
 }
