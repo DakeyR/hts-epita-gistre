acd778c4651dea3d93f883e0429dad2c9f2241fc
Raphael Gault <raphael.gault@epita.fr>
2018-02-06 17:48:36 +0100

    readelf: Check elf magic number

--- readelf/src/readelf.c
+++ readelf/src/readelf.c
@@ -3,7 +3,33 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include "readelf.h"
+
+int is_elf(unsigned char *ident)
+{
+  return ident[0] == 0x7f && ident[1] == 'E' && ident[2] == 'L'
+    && ident[3] == 'F';
+}
+
+void dump_header(ElfW(Ehdr) *header)
+{
+//  printf("{");
+  dump_entry(header, e_type, "%d,");
+/*  dump_entry(header, e_machine, "%d,");
+  dump_entry(header, e_version, "%d,");
+  dump_entry(header, e_entry, "%d,");
+  dump_entry(header, e_phoff, "%d,");
+  dump_entry(header, e_shoff, "%d,");
+  dump_entry(header, e_flags, "%d,");
+  dump_entry(header, e_ehsize, "%d,");
+  dump_entry(header, e_phentsize, "%d,");
+  dump_entry(header, e_phnum, "%d,");
+  dump_entry(header, e_shentsize, "%d,");
+  dump_entry(header, e_shnum, "%d,");
+  dump_entry(header, e_shstrndx, "%d,");*/
+//  printf("}");
+}
 
 void *load_header(int fd, off_t size)
 {
@@ -36,7 +62,15 @@ int main(int argc, char *argv[])
 
   ElfW(Ehdr) *header = load_header(fd, buf.st_size);
 
+  printf("%s\n", header->e_ident);
+  int ret = is_elf(header->e_ident);
+  if (!ret)
+    return 4;
+
+  dump_header(header);
+
   munmap(header, buf.st_size);
+  close(fd);
 
   return 0;
 }
