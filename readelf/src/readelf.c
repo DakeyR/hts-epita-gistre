880a49de10ea48849d4eface7fbf4df476eb26ae
Raphael Gault <raphael.gault@epita.fr>
2018-02-10 20:37:08 +0100

    readelf: Print symbols and macro's name or default for st_shndx field

--- readelf/src/readelf.c
+++ readelf/src/readelf.c
@@ -13,6 +13,10 @@
 
 #include "sh_type.h"
 
+#include "p_type.h"
+
+#include "st_shndx.h"
+
 int is_elf(unsigned char *ident)
 {
   return ident[0] == 0x7f && ident[1] == 'E' && ident[2] == 'L'
@@ -45,15 +49,33 @@ void *get_strtab(ElfW(Ehdr) *header)
   return (char *)header + shdr->sh_offset;
 }
 
-void dump_shdrs(ElfW(Shdr) *shdrs, unsigned int shnum, char *strtab)
+void dump_shdrs(ElfW(Shdr) *shdrs, unsigned int shnum, char *strtab,
+                             ElfW(Shdr) **dynsym, ElfW(Shdr) **symtab)
 {
   printf("\"sections\":[");
-  for (unsigned i = 0; i < shnum - 1; i++)
+  for (ElfW(Half) i = 0; i < shnum - 1; i++)
   {
+    if ((shdrs + i)->sh_type == SHT_DYNSYM)
+      *dynsym = (shdrs + i);
+    else if ((shdrs + i)->sh_type == SHT_SYMTAB)
+      *symtab = (shdrs + i);
+
     dump_section_header(shdrs + i, strtab);
     printf(",");
   }
   dump_section_header(shdrs + (shnum - 1), strtab);
+  printf("]\n");
+}
+
+void dump_phdrs(ElfW(Phdr) *phdrs, unsigned int phnum)
+{
+  printf("\"segments\":[");
+  for (ElfW(Half) i = 0; i < phnum - 1; i++)
+  {
+    dump_program_header(phdrs + i);
+    printf(",");
+  }
+  dump_program_header(phdrs + (phnum - 1));
   printf("]\n");
 }
 
@@ -73,6 +95,66 @@ void dump_section_header(ElfW(Shdr) *shead, char *strtab)
   printf("\"name\":\"%s\"", strtab + shead->sh_name);
   printf("}\n");
 }
+
+void dump_program_header(ElfW(Phdr) *phead)
+{
+  printf("{");
+  dump_macro(phead, p_type, "\"%s\",");
+  dump_entry(phead, p_offset, "%lu,");
+  dump_entry(phead, p_vaddr, "%lu,");
+  dump_entry(phead, p_paddr, "%lu,");
+  dump_entry(phead, p_filesz, "%lu,");
+  dump_entry(phead, p_memsz, "%lu,");
+  dump_entry(phead, p_flags, "%u,");
+  dump_entry(phead, p_align, "%lu");
+  printf("}\n");
+}
+
+void dump_symbols(ElfW(Shdr) *shead, ElfW(Ehdr) *header, char *strtab)
+{
+  ElfW(Shdr) *tmp = (void *)((char *)header + header->e_shoff);
+  char *local_strtab = (char *)header + tmp[shead->sh_link].sh_offset;
+
+  printf("{\"name\":\"%s\",", strtab + shead->sh_name);
+  printf("\"data\":[\n");
+  ElfW(Xword) size = shead->sh_size;
+  size = size / sizeof (ElfW(Sym));
+
+
+  ElfW(Sym) *symtab = (void *)((char *)header + shead->sh_offset);
+
+  for (ElfW(Xword) i = 0; i < size - 1; i++)
+  {
+    ElfW(Sym) *tmp = symtab + i;
+    printf("{");
+    dump_entry(tmp, st_name, "%u,");
+    const char *str = xlookup(st_shndx, tmp->st_shndx);
+    if (str)
+      dump_macro_dflt(str, st_shndx, "\"%s\",");
+    else
+      dump_macro_dflt(tmp->st_shndx, st_shndx, "%u,");
+    dump_entry(tmp, st_value, "%lu,");
+    dump_entry(tmp, st_size, "%lu,");
+    printf("\"name\": \"%s\"", local_strtab + tmp->st_name);
+    printf("},\n");
+  }
+
+  symtab = symtab + (size - 1);
+
+  printf("{");
+  dump_entry(symtab, st_name, "%u,");
+  const char *str = xlookup(st_shndx, symtab->st_shndx);
+  if (str)
+    dump_macro_dflt(str, st_shndx, "\"%s\",");
+  else
+    dump_macro_dflt(symtab->st_shndx, st_shndx, "%u,");
+  dump_entry(symtab, st_value, "%lu,");
+  dump_entry(symtab, st_size, "%lu,");
+  printf("\"name\": \"%s\"", local_strtab + symtab->st_name);
+  printf("}\n");
+  printf("]}");
+}
+
 
 void *load_header(int fd, off_t size)
 {
@@ -110,9 +192,32 @@ int main(int argc, char *argv[])
   char *strtab = get_strtab(header);
 
   printf("{\n");
+
   dump_header(header);
   printf(",");
-  dump_shdrs((void *)((char *)header + header->e_shoff), header->e_shnum, strtab);
+
+  ElfW(Shdr) *dynsym = NULL;
+  ElfW(Shdr) *symtab = NULL;
+
+  dump_shdrs((void *)((char *)header + header->e_shoff),
+               header->e_shnum, strtab, &dynsym, &symtab);
+  printf(",");
+
+  dump_phdrs((void *)((char *)header + header->e_phoff), header->e_phnum);
+  printf(",");
+
+  printf("\"symbols\":[");
+  if (dynsym)
+  {
+    dump_symbols(dynsym, header, strtab);
+    if (symtab)
+      printf(",");
+  }
+  if (symtab) {
+    dump_symbols(symtab, header, strtab);
+  }
+  printf("]");
+
   printf("}");
 
   munmap(header, buf.st_size);
--- readelf/src/readelf.h
+++ readelf/src/readelf.h
@@ -7,10 +7,17 @@
 #define dump_macro(Ptr, Field, Format) \
   printf("\"" #Field "\":" Format, xlookup(Field, (Ptr)->Field))
 
+#define dump_macro_dflt(val, Field, Format) \
+  printf("\"" #Field "\":" Format, val)
+
 int is_elf(unsigned char *ident);
 void dump_header(ElfW(Ehdr) *header);
 void *get_strtab(ElfW(Ehdr) *header);
-void dump_shdrs(ElfW(Shdr) *shdrs, unsigned int shnum, char *strtab);
+void dump_shdrs(ElfW(Shdr) *shdrs, unsigned int shnum, char *strtab,
+                             ElfW(Shdr) **dynsym, ElfW(Shdr) **symtab);
+void dump_phdrs(ElfW(Phdr) *phdrs, unsigned int phnum);
 void dump_section_header(ElfW(Shdr) *shead, char *strtab);
+void dump_program_header(ElfW(Phdr) *phead);
+void dump_symbols(ElfW(Shdr) *shead, ElfW(Ehdr) *header, char *strtab);
 void *load_header(int fd, off_t size);
 void *retrieve_header();
